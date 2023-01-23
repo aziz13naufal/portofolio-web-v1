94b035840c05df45b2c58075f02fb76961e91160
Aziz Naufal <aziz13naufal@gmail.com>
2023-01-23 21:16:04 +0700

    menghapus file app.js, menambah foto untuk halaman skills dan menambah sintak halaman skills

--- README.txt
+++ README.txt
@@ -1,11 +1,1 @@
 ini adalah website portofolio yang mengikuti desain Taufik dan Doni
-
-Command Git Push :
-git remote add origin https://github.com/aziz13naufal/portofolio-website.git
-git branch -M main
-git push -u origin main
-
-Note sementara:
-1. Menginstall Express JS
-2. Membuat file app.js dan memindahkan ke folder root
-3. Menginstall nodemon
--- dist/js/script.js
+++ dist/js/script.js
@@ -7,6 +7,10 @@ hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('hidden');
 });
 
+navMenu.addEventListener('click', () => {
+  navMenu.classList.add('hidden');
+  hamburger.classList.remove('hamburger-active');
+});
 
 // Dark Mode
 const darkToggle = document.querySelector('#dark-toggle');
@@ -21,3 +25,88 @@ darkToggle.addEventListener('click', () => {
     localStorage.theme = 'light';
    }
 });
+
+// Pindahkan posisi toggle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+    darkToggle.checked = true;
+  } else {
+    document.documentElement.classList.remove('dark')
+    darkToggle.checked = false;
+  }
+
+  // Skills
+  const languageToggle = document.querySelector('#language-toggle');
+  const frameworkToggle = document.querySelector('#framework-toggle');
+  const uiFrameworkToggle = document.querySelector('#ui-framework-toggle');
+  const dbmsToggle = document.querySelector('#dbms-toggle');
+  const allToggle = document.querySelector('#all-toggle');
+
+  // SKills Items
+  const HTML = document.querySelector('#HTML');
+  const CSS = document.querySelector('#CSS');
+  const JS = document.querySelector('#JS');
+  const NODE = document.querySelector('#NODE');
+  const EXPRESS = document.querySelector('#EXPRESS');
+  const TAILWIND = document.querySelector('#TAILWIND');
+  const MONGO= document.querySelector('#MONGO');
+  const MYSQL = document.querySelector('#MYSQL');
+
+  // Language
+  languageToggle.addEventListener('click', () => {
+    HTML.classList.remove('hidden');
+    CSS.classList.remove('hidden');
+    JS.classList.remove('hidden');
+    NODE.classList.add('hidden');
+    EXPRESS.classList.add('hidden');
+    TAILWIND.classList.add('hidden');
+    MONGO.classList.add('hidden');
+    MYSQL.classList.add('hidden');
+  });
+
+  // Framework
+  frameworkToggle.addEventListener('click', () => {
+    HTML.classList.add('hidden');
+    CSS.classList.add('hidden');
+    JS.classList.add('hidden');
+    NODE.classList.remove('hidden');
+    EXPRESS.classList.remove('hidden');
+    TAILWIND.classList.add('hidden');
+    MONGO.classList.add('hidden');
+    MYSQL.classList.add('hidden');
+  });
+
+  // UI Framework
+  uiFrameworkToggle.addEventListener('click', () => {
+    HTML.classList.add('hidden');
+    CSS.classList.add('hidden');
+    JS.classList.add('hidden');
+    NODE.classList.add('hidden');
+    EXPRESS.classList.add('hidden');
+    TAILWIND.classList.remove('hidden');
+    MONGO.classList.add('hidden');
+    MYSQL.classList.add('hidden');
+  });
+
+  // DBMS
+  dbmsToggle.addEventListener('click', () => {
+    HTML.classList.add('hidden');
+    CSS.classList.add('hidden');
+    JS.classList.add('hidden');
+    NODE.classList.add('hidden');
+    EXPRESS.classList.add('hidden');
+    TAILWIND.classList.add('hidden');
+    MONGO.classList.remove('hidden');
+    MYSQL.classList.remove('hidden');
+  })
+
+  allToggle.addEventListener('click', () => {
+    HTML.classList.remove('hidden');
+    CSS.classList.remove('hidden');
+    JS.classList.remove('hidden');
+    NODE.classList.remove('hidden');
+    EXPRESS.classList.remove('hidden');
+    TAILWIND.classList.remove('hidden');
+    MONGO.classList.remove('hidden');
+    MYSQL.classList.remove('hidden');
+  })
